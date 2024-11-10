16307ba2dc8226bb24caceec0b6139f8c0bcfce5
Shirou-kun <shivanshs9@gmail.com>
2024-11-11 01:00:04 +0900

    chore: brand revise

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,16 +12,17 @@ function App() {
         <div className='flex justify-center items-center min-h-screen'>
             <div className='container flex flex-col justify-center items-center'>
                 <h1 className='text-5xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Whisper WebGPU
+                    Whisper Notes
                 </h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    ML-powered speech recognition directly in your browser
+                    Write Notes on the Go! ML-powered speech recognition directly in your browser.
                 </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
             </div>
 
             <div className='absolute bottom-4'>
+                <p>
                 Made with{" "}
                 <a
                     className='underline'
@@ -29,6 +30,16 @@ function App() {
                 >
                     🤗 Transformers.js
                 </a>
+                </p>
+                <p>
+                Developed by{" "}
+                <a
+                    className='underline'
+                    href='https://github.com/shivanshs9'
+                >
+                    @shivanshs9
+                </a>
+                </p>
             </div>
         </div>
     ) : (
