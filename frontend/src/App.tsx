ad779fdb2f3c59c47abfa869cacd865729396222
Shirou-kun <shivanshs9@gmail.com>
2024-11-11 01:06:01 +0900

    ft: move backend to same domain under /api path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,8 @@ function App() {
                     Whisper Notes
                 </h1>
                 <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    Write Notes on the Go! ML-powered speech recognition directly in your browser.
+                    Write Notes on the Go! <br />
+                    ML-powered speech recognition directly in your browser.
                 </h2>
                 <AudioManager transcriber={transcriber} />
                 <Transcript transcribedData={transcriber.output} />
